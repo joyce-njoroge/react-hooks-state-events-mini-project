18cc6a1f2d343a8b58db915d0a3c17a1a5399009
Joyce Njoroge <joycewambuku33@gmail.com>
2023-04-24 09:04:23 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+
+  const handleDeleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter categories={CATEGORIES} />
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={tasks} onDelete={handleDeleteTask} />
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories }) {
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category) => (
+        <button key={category} onClick={() => console.log(`Clicked ${category}`)}>
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
